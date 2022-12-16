3488c3920e210cae51376d325247277afc436dd7
cat man <109460706+mrcottonball@users.noreply.github.com>
2022-12-16 16:46:18 +0000

    Create variables_in_rust.rs

---
+++ variables_in_rust.rs
@@ -0,0 +1,10 @@
+#variables in rust are really cool
+fn main() {
+  let x = 6;
+  #x is uneditable
+  #if we tried to do → x = 8; it would give an error
+  
+  let mut y = 5;
+  #adding mut means we can now edit the variable
+  y = 89; #this would work
+}
